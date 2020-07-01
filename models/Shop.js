e88119a9a38c1c0702f49090e506ddac736cb231
Javlonbek Rakhimberdiev <rjavlonbek@Javlonbeks-MacBook-Pro.local>
2020-07-01 18:13:59 +0500

    added shop model

---
+++ models/Shop.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Types = mongoose.Schema.Types;
+
+var ShopSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    slug:{
+        type: String,
+        slug: 'name',
+        unique: true
+    },
+    products: [{
+        product: {
+            type: Types.ObjectId
+        },
+        quantity: {
+            type: Number
+        }
+    }],
+    phone: {
+        type: String,
+        default: ''
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    address2: {
+        type: String
+    },
+    preview_text: {
+        type: String,
+        default: ''
+    },
+    description:{
+        type:String,
+        default: ''
+    },
+    active:{
+        type: Boolean,
+        default: true
+    },
+    order: {
+        type:Number,
+        default: 0
+    },
+    image: {
+        type: String
+    },
+    created_at: {
+        type:Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date
+    }
+}, {
+    minimize: false
+});
+
+module.exports = mongoose.model('Shop', ShopSchema);
+
