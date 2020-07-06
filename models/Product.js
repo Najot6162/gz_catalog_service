9207fa1465fd723d127d548c42a80765eeece99b
abrorkhamidov <khamidov10abror98@mail.ru>
2020-07-06 10:00:55 +0500

    New fields in product model and proto in gz_catalog_service

--- models/Product.js
+++ models/Product.js
@@ -14,11 +14,11 @@ var ProductSchema = new mongoose.Schema({
         type: Types.ObjectId,
         ref: 'Category'
     },
-    additionalCategories: [{
+    additional_categories: [{
         type: Types.ObjectId,
         ref: 'Category'
     }],
-    relatedProducts: [{
+    related_products: [{
         type: Types.ObjectId,
         ref: 'Product'
     }],
@@ -65,11 +65,10 @@ var ProductSchema = new mongoose.Schema({
         type: Boolean,
         default: true
     },
-<<<<<<< HEAD
     external_id: {
         type: Number,
         default: null
-=======
+    },
     meta: {
         title: {
             type: String,
@@ -83,7 +82,6 @@ var ProductSchema = new mongoose.Schema({
             type: String,
             default: ''
         }
->>>>>>> fac0216e4a6f8352b9678b90bbacaf203828366d
     },
     created_at: {
         type: Date,
