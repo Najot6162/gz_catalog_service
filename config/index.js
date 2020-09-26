70bdf12a80307d548add1b0967db1f26cd9c77b3
rjavlonbek <javlonarjei042@gmail.com>
2020-09-26 17:11:45 +0500

    cloud url prefix changed

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const config = {
     mongoPassword: getConf('MONGO_PASSWORD', 'mysecret'),
     mongoDatabase: getConf('MONGO_DATABASE', 'catalog_service'),
     RPCPort: getConf('RPC_PORT', 7000),
-    cloudUrl: getConf('CLOUD_URL', 'https://sdn.delever.uz/goodzone/'),
+    cloudUrl: getConf('CLOUD_URL', 'https://cdn.delever.uz/goodzone/'),
     lang: getConf('DEFAULT_LANG', 'ru')
 }
 
