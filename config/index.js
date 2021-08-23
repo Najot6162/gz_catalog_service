b993ec42ba0f9a83de0211ecefac585725c47ebc
Biloliddin <najot.6162@gmail.com>
2021-08-23 11:59:51 +0000

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ const config = {
     minioPort: getConf('MINIO_PORT', 9001),
     minioAccessKeyID: getConf('MINIO_ACCESS_KEY', 'YdrxMxxWQ4B6QXXYYGJbFM6EGttGKCBpUL2qrkV3nV6Kd8Gy5ajHqjy4aqpjtACT'),
     minioSecretAccesKey: getConf('MINIO_SECRET_KEY', 'Hfdxjym6QUjaPUaS2ygf3754xdPRHVunbXhRvncaQXXrnG2kg4dYaquc8WN6KgnJ'),
-    ruleServiceHost: getConf('RuleServiceHost', "rules_service"),
-    ruleServicePort: getConf('RuleServicePort',"7007")
+    ruleServiceHost: getConf('RULE_SERVICE_HOST', "rules_service"),
+    ruleServicePort: getConf('RULE_SERVICE_PORT',"7007")
 }
 
 function getConf(name, def = ''){
